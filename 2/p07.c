0434da84fe07c754dce144fbf7d27eec55a6c600
anasbouc <anasbouc1@mbg.duth.gr>
2022-12-30 17:02:40 +0200

    first commit

---
+++ 2/p07.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <math.h> 
+
+int main()
+{
+	char k;
+	char x;
+	char y;
+
+
+	while ((scanf ("%c%c%c", &k, &x, &y) == 3))
+	{
+		printf("%c%c%c\n", k, x, y);
+	} 
+
+
+}
